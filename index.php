fcecf206b75aa7f31f6912931256129ab688cfc8
Laura <lauramorenoramos97@gmail.com>
2021-10-22 10:23:29 +0200

    2Commit CSS-Return

--- index.php
+++ index.php
@@ -14,35 +14,30 @@
     $_SESSION['url'] = 'index.php';
     ?>
     <main class="container gamer">
-    <header class="row gamer">
-            <div class="row">
-                <div class="l-col-12 m-col-12 s-col-12">
-                    <h1>ESCAPE WEB</h1>
-                </div>
-            </div>
-            <div class="row">
-                <div class="l-col-12 m-col-12 s-col-12">
-                    <h4>Tu pagina de scape room</h4>
-                </div>
+        <header class="row gamer">
+
+            <div class="xl-col-12 l-col-12 m-col-12 s-col-12">
+                <h1>ESCAPE WEB</h1>
+                <h4>Tu pagina de scape room</h4>
             </div>
         </header>
 
         <section class="row">
-        <div class="row">
-            <div class="l-col-12 m-col-12 s-col-12 separado">
-                <h2>Login:</h2>
+            <div class="row">
+                <div class="l-col-12 m-col-12 s-col-12 separado">
+                    <h2>Login:</h2>
+                </div>
             </div>
-        </div>
 
             <form action="controlador.php" method="POST" class="gamer">
                 <div class="row">
-                        <label class="l-col-3 m-col-3 s-col-3 ">Email:</label>
-                        <input class=" l-col-3 m-col-3 s-col-3 " type="text" value="" name="Email" placeholder="Inserta tu Email" require>
+                    <label class="l-col-3 m-col-3 s-col-3 ">Email:</label>
+                    <input class=" l-col-3 m-col-3 s-col-3 " type="text" value="" name="Email" placeholder="Inserta tu Email" require>
                 </div>
 
                 <div class="row">
-                        <label class="l-col-3 m-col-3 s-col-3">Contraseña:</label>
-                        <input class=" l-col-3 m-col-3 s-col-3" type="text" value="" name="Password" placeholder="Inserta tu Contraseña" require>
+                    <label class="l-col-3 m-col-3 s-col-3">Contraseña:</label>
+                    <input class=" l-col-3 m-col-3 s-col-3" type="text" value="" name="Password" placeholder="Inserta tu Contraseña" require>
                 </div>
 
                 <div class="row">
@@ -51,13 +46,13 @@
                     </div>
                     <div class="l-col-2 m-col-2 s-col-2 ">
                         <input type="submit" value="Registrarse" name="Registrarse">
-                </div>
-
-                <div class="row">
-                <div class=" l-col-12 m-col-12 s-col-12">
-                        <a class='gamer' href="./password.php">Se me ha olvidado la contraseña...</a>
                     </div>
+
                     <div class="row">
+                        <div class=" l-col-12 m-col-12 s-col-12">
+                            <a class='gamer' href="./password.php">Se me ha olvidado la contraseña...</a>
+                        </div>
+                        <div class="row">
             </form>
         </section>
         <footer class=" gamer row">
