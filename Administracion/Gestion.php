3c1c6ac7a038ccfd526544c0ddd090c8aaa8fe0c
LauraMR97 <lauramorenoramos97@gmail.com>
2021-11-02 21:21:57 +0100

    Cambios en Gestion CSS

--- Administracion/Gestion.php
+++ Administracion/Gestion.php
@@ -25,136 +25,128 @@
         </header>
 
         <section class="row">
+            <div class="col-5 margen-3 s-margen-0 s-col-12">
+                <div class="row">
+                    <div class="xl-col-12 l-col-12 m-col-12 s-col-12 separado">
+                        <h3>Gestion:</h3>
+                    </div>
+                </div>
+                <?php
+                if ($_SESSION['Eleccion'] == 'Aniadir') {
+                ?>
 
-            <div class="xl-col-12 l-col-12 m-col-12 s-col-12 separado">
-                <h3>Gestion:</h3>
+                    <div class="row">
+                        <div class="xl-col-12 l-col-12 m-col-12 s-col-12">
+                            <form action="../Base_de_datos/controlador.php" method="POST" class="oriental" novalidate>
+                                <input type="hidden" name="recaptcha_response" id="recaptchaResponse">
+                                <div class="row p-a-1">
+                                    <div class="col-4">
+                                        <input type="checkbox" id="Admin" name="tipousur[]" value="Ad">Admin
+                                    </div>
+                                    <div class="col-4">
+                                        <input type="checkbox" id="Edit" name="tipousur[]" value="Ed">Editor
+                                    </div>
+                                    <div class="col-4">
+                                        <input type="checkbox" id="User" name="tipousur[]" value="Us">Usuario
+                                    </div>
+                                </div>
+
+                                <div class="row p-a-1">
+                                    <label class="col-6">Nombre:</label>
+                                    <input class="col-6" type="text" value="" id="nom" name="Nombre" placeholder="Inserta un Nombre" required>
+                                    <span class="error" id="nombreError" aria-live="polite"></span>
+                                </div>
+
+                                <div class="row p-a-1">
+                                    <label class="col-6">Email:</label>
+                                    <input class="col-6" type="email" value="" id="mail" name="Email" placeholder="Inserta un Email" required>
+                                    <span class="error" aria-live="polite"></span>
+                                </div>
+
+                                <div class="row p-a-1">
+                                    <label class="col-6">Contraseña:</label>
+                                    <input class="col-6" type="text" value="" id="password" name="Password" placeholder="Inserta una Contraseña" required>
+                                    <span id="passwordError" class="error" aria-live="polite"></span>
+                                </div>
+
+                                <div class="row p-a-1">
+                                    <label class="col-6">Confirmar Contraseña:</label>
+                                    <input class="col-6" type="text" value="" id="passwordConfirm" name="PasswordRepeat" placeholder="Inserta la Contraseña de nuevo" required>
+                                    <span class="error" id="passwordConfirmError" aria-live="polite"></span>
+                                </div>
+
+                                <div class="row p-a-1">
+                                    <button class="col-6 s-col-12" type="submit" name='Editar' disabled><img src="../ICONOS/edit.png" class="tamaño"></button>
+                                    <button class="col-6 s-col-12" type="submit" id="Registro" name='ADD'><img src="../ICONOS/add.png" class="tamaño"></button>
+                                </div>
+                                <div class="row p-a-1">
+                                    <input class="col-6 s-col-12 botVolver" type="submit" value="Volver" name="VolverAdministracion">
+                                    <input class="col-6 s-col-12" type="submit" value="Cerrar Sesion" name="CerrarSesion">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                <?php
+                } else {
+                    $perAnt = Conexion::buscarPersonaPorCorreo($_SESSION['email']);
+                    $rol = Conexion::verRol($perAnt->getEmail());
+                ?>
+                    <div class="row">
+                        <div class="xl-col-12 l-col-12 m-col-12 s-col-12">
+                            <form action="../Base_de_datos/controlador.php" method="POST" class="oriental" novalidate>
+                                <input type="hidden" name="recaptcha_response" id="recaptchaResponse">
+
+                                <div class="row p-a-1">
+                                    <div class="col-4">
+                                        <input type="checkbox" id="Admin" name="tipousur[]" value="Ad">Admin
+                                    </div>
+                                    <div class="col-4">
+                                        <input type="checkbox" id="Edit" name="tipousur[]" value="Ed">Editor
+                                    </div>
+                                    <div class="col-4">
+                                        <input type="checkbox" id="User" name="tipousur[]" value="Us">Usuario
+                                    </div>
+                                </div>
+
+                                <div class="row p-a-1">
+                                    <label class="col-6">Nombre:</label>
+                                    <input class="col-6" type="text" value="<?php echo $perAnt->getNombre() ?>" id="nom" name="Nombre" placeholder="Inserta un Nombre" required>
+                                    <span class="error" id="nombreError" aria-live="polite"></span>
+                                </div>
+
+
+                                <div class="row p-a-1">
+                                    <label class="col-6">Email:</label>
+                                    <input class="col-6" type="email" value="<?php echo $perAnt->getEmail() ?>" id="mail" name="Email" placeholder="Inserta un Email" required>
+                                    <span class="error" aria-live="polite"></span>
+                                </div>
+
+                                <div class="row p-a-1">
+                                    <label class="col-6">Contraseña:</label>
+                                    <input class="col-6" type="password" value="<?php echo $perAnt->getPassword() ?>" name="Password" placeholder="Inserta una Contraseña" readonly>
+                                </div>
+
+                                <div class="row p-a-1">
+                                    <label class="col-6">Confirmar Contraseña:</label>
+                                    <input class="col-6" type="password" value="<?php echo $perAnt->getPassword() ?>" name="PasswordRepeat" placeholder="Inserta la Contraseña de nuevo" readonly>
+                                </div>
+
+                                <div class="row p-a-1">
+                                    <button class="col-6 s-col-12" type="submit" id="Edita" name='Editar'><img src="../ICONOS/edit.png" class="tamaño"></button>
+                                    <button class="col-6 s-col-12" type="submit" name='ADD' disabled><img src="../ICONOS/add.png" class="tamaño"></button>
+                                </div>
+                                <div class="row p-a-1">
+                                    <input class="col-6 s-col-12 botVolver" type="submit" value="Volver" name="VolverAdministracion">
+                                    <input class="col-6 s-col-12" type="submit" value="Cerrar Sesion" name="CerrarSesion">
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                <?php
+                }
+                ?>
             </div>
-
-            <?php
-            if ($_SESSION['Eleccion'] == 'Aniadir') {
-            ?>
-                <form action="../Base_de_datos/controlador.php" method="POST" class="oriental" novalidate>
-                    <input type="hidden" name="recaptcha_response" id="recaptchaResponse">
-                    <div class="row">
-                        <div class=" margen-5 l-col-3 m-col-3 s-col-3 separado">
-                            <label>Nombre:</label>
-                            <input type="text" value="" id="nom" name="Nombre" placeholder="Inserta un Nombre" required>
-                            <span class="error" id="nombreError" aria-live="polite"></span>
-                        </div>
-                        <div class="l-col-3 m-col-3 s-col-3 separado">
-                            <label><input type="checkbox" id="Admin" name="tipousur[]" value="Ad">Administrador</label>
-                            <label><input type="checkbox" id="Edit" name="tipousur[]" value="Ed">Editor</label>
-                            <label><input type="checkbox" id="User" name="tipousur[]" value="Us">Usuario</label>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="margen-5 l-col-3 m-col-3 s-col-3 separadoPequeño">
-                            <label>Email:</label>
-                            <input type="email" value="" id="mail" name="Email" placeholder="Inserta un Email" required>
-                            <span class="error" aria-live="polite"></span>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="margen-5 l-col-3 m-col-3 s-col-3 separadoPequeño">
-                            <label>Contraseña:</label>
-                            <input type="text" value="" id="password" name="Password" placeholder="Inserta una Contraseña" required>
-                            <span id="passwordError" class="error" aria-live="polite"></span>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="margen-5 l-col-3 m-col-3 s-col-3 separadoPequeño">
-                            <label>Confirmar Contraseña:</label>
-                            <input type="text" value="" id="passwordConfirm" name="PasswordRepeat" placeholder="Inserta la Contraseña de nuevo" required>
-                            <span class="error" id="passwordConfirmError" aria-live="polite"></span>
-                        </div>
-
-                    </div>
-
-                    <div class="l-col-2 m-col-2 s-col-2">
-                        <button type="submit" name='Editar' disabled><img src="../ICONOS/edit.png" class="tamaño"></button>
-                    </div>
-
-                    <div class="l-col-2 m-col-2 s-col-2">
-                        <button type="submit" id="Registro" name='ADD'><img src="../ICONOS/add.png" class="tamaño"></button>
-                    </div>
-
-
-                    <div class="l-col-2 m-col-2 s-col-2">
-                        <input type="submit" value="Volver" name="VolverAdministracion">
-                    </div>
-
-                    <div class="l-col-2 m-col-2 s-col-2 ">
-                        <input type="submit" value="Cerrar Sesion" name="CerrarSesion">
-                    </div>
-                    </div>
-                </form>
-            <?php
-            } else {
-                $perAnt = Conexion::buscarPersonaPorCorreo($_SESSION['email']);
-                $rol = Conexion::verRol($perAnt->getEmail());
-            ?>
-                <form action="../Base_de_datos/controlador.php" method="POST" class="oriental" novalidate>
-                    <div class="row">
-                        <div class=" margen-5 l-col-3 m-col-3 s-col-3 separado">
-                            <label>Nombre:</label>
-                            <input type="text" id="nom" value="<?php echo $perAnt->getNombre() ?>" name="Nombre" placeholder="Inserta un Nombre" required>
-                            <span class="error" id="nombreError" aria-live="polite"></span>
-                        </div>
-                        <div class="l-col-3 m-col-3 s-col-3 separado">
-                            <label><input type="checkbox" id="Admin" name="tipousur[]" <?php echo (isset($rol[0]) && ($rol[0] == 0)) || (isset($rol[1]) && ($rol[1] == 0)) || (isset($rol[2]) && ($rol[2] == 0)) ? ' checked' : ' '; ?> value="Ad">Administrador</label>
-                            <label><input type="checkbox" id="Edit" name="tipousur[]" <?php echo (isset($rol[0]) && ($rol[0] == 1)) || (isset($rol[1]) && ($rol[1] == 1)) || (isset($rol[2]) && ($rol[2] == 1)) ? ' checked' : ' '; ?> value="Ed">Editor</label>
-                            <label><input type="checkbox" id="User" name="tipousur[]" <?php echo (isset($rol[0]) && ($rol[0] == 2)) || (isset($rol[1]) && ($rol[1] == 2)) || (isset($rol[2]) && ($rol[2] == 2)) ? ' checked' : ' '; ?> value="Us">Usuario</label>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="margen-5 l-col-3 m-col-3 s-col-3 separadoPequeño">
-                            <label>Email:</label>
-                            <input type="email" id="mail" value="<?php echo $perAnt->getEmail() ?>" name="Email" placeholder="Inserta un Email" required>
-                            <span class="error" aria-live="polite"></span>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="margen-5 l-col-3 m-col-3 s-col-3 separadoPequeño">
-                            <label>Contraseña:</label>
-                            <input type="password" value="<?php echo $perAnt->getPassword() ?>" name="Password" placeholder="Inserta una Contraseña" readonly>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="margen-5 l-col-3 m-col-3 s-col-3 separadoPequeño">
-                            <label>Confirmar Contraseña:</label>
-                            <input type="password" value="<?php echo $perAnt->getPassword() ?>" name="PasswordRepeat" placeholder="Inserta la Contraseña de nuevo" readonly>
-                        </div>
-
-                    </div>
-
-                    <div class="l-col-2 m-col-2 s-col-2">
-                        <button type="submit" id="Edita" name='Editar'><img src="../ICONOS/edit.png" class="tamaño"></button>
-
-                    </div>
-
-                    <div class="l-col-2 m-col-2 s-col-2">
-                        <button type="submit" name='ADD' disabled><img src="../ICONOS/add.png" class="tamaño"></button>
-                    </div>
-
-
-                    <div class="l-col-2 m-col-2 s-col-2">
-                        <input class="botVolver" type="submit" value="Volver" name="VolverAdministracion">
-                    </div>
-
-                    <div class="l-col-2 m-col-2 s-col-2 ">
-                        <input type="submit" value="Cerrar Sesion" name="CerrarSesion">
-                    </div>
-                    </div>
-                </form>
-            <?php
-            }
-            ?>
         </section>
         <footer class=" oriental row">
             <div class="xl-col-12 l-col-12 m-col-12 s-col-12">
